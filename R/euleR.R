8f18e2873857ca43f2da3d64b7bc9cdb77fa8ffd
Aidan Delaney <aidan@ontologyengineering.org>
2015-11-10 10:32:26 +0000

    Deal with updated JSON format passed back from K-bridge

--- R/euleR.R
+++ R/euleR.R
@@ -2,18 +2,18 @@ euleR <- function (combinations, url, ...) {
   if(missing(combinations)) {
     stop("combinations must be specified")
   }
-  
+
   j <- foreach(i = 1:length(combinations)) %do% combinations[i]
   l <- list("area_specifications" = j)
   json <- toJSON(l)
   print(json)
-  
+
   httpheader <- c(Accept="application/json; charset=UTF-8",
                   "Content-Type"="application/json")
-  
+
   if(missing(url)) {
     # if you don't provide a URL we'll use a test one.
-    r <- postForm('http://localhost:8080', .opts=list(httpheader=httpheader
+    r <- postForm('http://localhost:8080/layout', .opts=list(httpheader=httpheader
                                                       ,postfields=json))
   } else {
     r <- postForm(url, .opts=list(httpheader=httpheader
@@ -24,18 +24,18 @@ euleR <- function (combinations, url, ...) {
 }
 
 plot.euleR <- function(d) {
-  max_radius <- max(sapply(d$circles, function (x) x$circle$r))
-  max_x <- max(sapply(d$circles, function (x) x$circle$xc))
-  min_x <- min(sapply(d$circles, function (x) x$circle$xc))
-  max_y <- max(sapply(d$circles, function (x) x$circle$yc))
-  min_y <- min(sapply(d$circles, function (x) x$circle$yc))
-  
+  max_radius <- max(sapply(d$circles, function (x) x$radius))
+  max_x <- max(sapply(d$circles, function (x) x$x))
+  min_x <- min(sapply(d$circles, function (x) x$x))
+  max_y <- max(sapply(d$circles, function (x) x$y))
+  min_y <- min(sapply(d$circles, function (x) x$y))
+
   # Create a large enough canvas
   emptyplot(xlim=c(min_x - max_radius, max_x + max_radius), ylim=c(min_y - max_radius, max_y + max_radius))
-  
-  sapply(d$circles, function (x) { 
-    filledcircle(r1=x$circle$r, mid=c(x$circle$xc, x$circle$yc), col=rgb(1,1,1,0), lcol="black")
-    text(x$circle$xc, x$circle$yc, x$label)
+
+  sapply(d$circles, function (x) {
+    filledcircle(r1=x$radius, mid=c(x$x, x$y), col=rgb(1,1,1,0), lcol="black")
+    text(x$x, x$y, x$label)
     })
 }
 
